af7059b750ff20c7949a5590a52b7fd08bfb6e7f
Christian <contact@komodo.link>
2024-03-13 20:28:12 +0100

    move interfaces to bottom

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,31 @@ export interface Env {
   API_KEY: string
 }
 
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    if (request.method !== "POST") return new Response("Method Not Allowed", { status: 405 })
+
+    const apiKey = request.headers.get("Authorization")?.split(" ")[1]
+    if (apiKey !== env.API_KEY) return new Response("Unauthorized", { status: 401 })
+
+    const email: EmailSendBody = await request.json()
+    email.personalizations = email.personalizations.map((personalization) => {
+      return {
+        ...personalization,
+        dkim_domain: env.DOMAIN,
+        dkim_private_key: env.DKIM_PRIVATE_KEY,
+        dkim_selector: "mailchannels"
+      }
+    })
+
+    return await fetch("https://api.mailchannels.net/tx/v1/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(email)
+    })
+  }
+}
+
 interface EmailContent {
   type: string;
   value: string;
@@ -37,29 +62,4 @@ interface EmailSendBody {
   personalizations: personalization[];
   reply_to?: EmailName;
   subject: string;
-}
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    if (request.method !== "POST") return new Response("Method Not Allowed", { status: 405 })
-
-    const apiKey = request.headers.get("Authorization")?.split(" ")[1]
-    if (apiKey !== env.API_KEY) return new Response("Unauthorized", { status: 401 })
-
-    const email: EmailSendBody = await request.json()
-    email.personalizations = email.personalizations.map((personalization) => {
-      return {
-        ...personalization,
-        dkim_domain: env.DOMAIN,
-        dkim_private_key: env.DKIM_PRIVATE_KEY,
-        dkim_selector: "mailchannels"
-      }
-    })
-
-    return await fetch("https://api.mailchannels.net/tx/v1/send", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(email)
-    })
-  }
 }
